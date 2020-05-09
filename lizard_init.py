3c1fdce6b46122e14ce183456ca0a619599556c8
Farhan  Rahman <farhanrahman02@gmail.com>
2020-05-10 04:04:59 +0530

    intialising the parameters

---
+++ lizard_init.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+np.uintt32 K[120]
+np.uint32 IV[64]
+np.uint32 z[128]
+np.uint8 L[KEYSTREAM_SIZE+128]
+np.uint8 Q[KEYSTREAM_SIZE+128]
+np.uint8 T[KEYSTREAM_SIZE+128]
+np.uint8 Ttilde[KEYSTREAM_SIZE+128]
+np.uint8 B[KEYSTREAM_SIZE+258][90]
+np.uint8 S[KEYSTREAM_SIZE+128][31]
+np.uint8 a257=0;
+int t = 0;
+np.uint8 [KEYSTREAM_SIZE+128]
+
